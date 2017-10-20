6546e3dce6ea87f1ab438840ce1638b01f5c8505
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2017-10-20 09:22:16 -0200

    ajustes imports

--- src/br/com/tqi/teste/TesteCriaBoletaSGAD.java
+++ src/br/com/tqi/teste/TesteCriaBoletaSGAD.java
@@ -2,12 +2,10 @@ package br.com.tqi.teste;
 
 import java.awt.AWTException;
 import java.io.IOException;
-import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
